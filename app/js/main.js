e3ad8f507f82dcf66b29594955fb9e8dfee8987f
Esutarosa <107695658+Esutarosa@users.noreply.github.com>
2023-07-27 23:41:01 +0300

    done header, fixed bugs

--- app/js/main.js
+++ app/js/main.js
@@ -13,3 +13,21 @@ if (navToggle && navClose) {
 }
 
 /*=============== DROPDOWN ===============*/
+
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () => {
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(x => x.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('bg-header')
+                       : header.classList.remove('bg-header')
+}
+window.addEventListener('scroll', scrollHeader)
