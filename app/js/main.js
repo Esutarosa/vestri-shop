1434fb2f261eb46aef1f3a54e9fecfc000a967bc
Esutarosa <107695658+Esutarosa@users.noreply.github.com>
2023-07-27 04:36:58 +0300

    changed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,41 +1,15 @@
 /*=============== SHOW MENU ===============*/
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-             nav = document.getElementById(navId)
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
 
-    toggle.addEventListener('click', () => {
-        nav.classList.toggle('show-menu')
-
-        toggle.classList.toggle('show-icon')
-    })
+const toggleNavMenu = () => {
+    navMenu.classList.toggle('show-menu');
 }
-showMenu('nav-toggle', 'nav-menu')
+
+if (navToggle && navClose) {
+    navToggle.addEventListener('click', toggleNavMenu);
+    navClose.addEventListener('click', toggleNavMenu);
+}
 
 /*=============== DROPDOWN ===============*/
-const dropdownItems = document.querySelectorAll('.dropdown__item')
-
-dropdownItems.forEach((item) => {
-    const dropdownHeader = item.querySelector('.dropdown-header')
-
-    dropdownHeader.addEventListener('click', () => {
-        const openItem = document.querySelector('.dropdown-open')
-
-        toggleItem(item)
-
-        if (openItem && openItem !== item) {
-             toggleItem(openItem)
-        }
-    })
-})
-
-const toggleItem = (item) => {
-    const dropdownContent = item.querySelector('.dropdown__menu')
-
-    if (item.classList.contains('dropdown-open')) {
-        dropdownContent.removeAttribute('style')
-        item.classList.remove('dropdown-open')
-    } else {
-        dropdownContent.style.height = dropdownContent.scrollHeight + 'px'
-        item.classList.add('dropdown-open')
-    }
-}
