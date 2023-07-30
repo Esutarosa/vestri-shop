334260a491de9ead37e2b9cfa99e07a4a1a5fdb4
Esutarosa <107695658+Esutarosa@users.noreply.github.com>
2023-07-30 23:37:48 +0300

    scroll up

--- app/js/main.js
+++ app/js/main.js
@@ -61,3 +61,11 @@ const featuredSwiper = new Swiper(".featured__swiper", {
         draggable: true,
     },
 })
+
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
